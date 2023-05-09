3da48aff17f3a768480710b69c90bd57a521004a
fzamora <flxzamora91>
2023-05-08 21:19:18 -0600

    Menu de navegacion para el dashboard de trading

---
+++ app/Informes.tsx
@@ -0,0 +1,17 @@
+import styles from './page.module.css'
+
+export default function Informes() {
+  
+    return (
+       
+        <main className={styles.main}>
+
+        <div className="container">
+
+          test  
+
+        </div>
+
+      </main>
+
+    )}
